e1896e448f7b84d443c06c095b00a099ec82cc29
Lian Hernandez <lian.hernandez91@gmail.com>
2025-03-15 20:43:34 -0300

    ok

---
+++ 05_tuples.py
@@ -0,0 +1,11 @@
+### Tuplas ###
+# Las tuplas son una secuencia de elementos que no pueden ser modificados
+my_tuple = tuple()
+my_tuple = ()
+my_tuple = (1,2,3,"ian",5)
+print(my_tuple)
+print(my_tuple[0]) # 1 
+
+my_tuple.index("ian") # 3 dice en que posicion esta
+
+# my_tuple[1] = 3 # error no se puede modificar
