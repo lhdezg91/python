18724bf9f47bc240a39ce59a322554430e551b00
Lian Hernandez <lian.hernandez91@gmail.com>
2025-03-14 22:14:43 -0300

    c4

--- 01_variables.py
+++ 01_variables.py
@@ -3,3 +3,12 @@ mi_variable = "hola"
 mi_variable_entera = 3
 
 mi_bool_variable = True
+
+
+print(mi_variable,mi_bool_variable,mi_variable_entera)
+
+str(mi_variable_entera)    # convierte a string
+int(mi_variable_entera)    # convierte a entero
+float(mi_variable_entera)  # convierte a flotante
+
+print(len("hola")) # longitud de la cadena
