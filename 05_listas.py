e436ac60445ec1d7f106d3b0dcf6e25f5ea79548
Lian Hernandez <lian.hernandez91@gmail.com>
2025-03-15 18:00:55 -0300

    listas

--- 05_listas.py
+++ 05_listas.py
@@ -14,6 +14,17 @@ list.append(8)
 list.append("ian")
 list.append("ok")
 
+list.insert(2, "hola") # insertar en la posicion 3
+
+list.remove(67) # remover si hay varios elimina el primero
+
+list.pop() # elimina el ultimo
+
+elemento = list.pop(2) # elimina el elemento en la posicion 2, devuelve el elemento eliminado
+
+del list[0] # elimina el elemento en la posicion 0
+
 print(list[-1]) # de atras para alante
 
 print(list.count("ian")) # cuantas veces aparece
+
