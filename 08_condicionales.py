874382dd49c5e0be1ebca0a23c05e6d44bb14ab9
Lian Hernandez <lian.hernandez91@gmail.com>
2025-03-16 19:48:25 -0300

    o

---
+++ 08_condicionales.py
@@ -0,0 +1,23 @@
+### Condicionales ###
+
+condicion = True
+
+if condicion:
+    print("Hola")
+else:
+    print("Adios")
+    
+suma = 5 + 5
+
+if suma == 10 and condicion:
+    print("Hola")
+elif suma != 10:
+    print("Adios")
+else:
+    print("No se cumple ninguna condicion")
+
+if 1:
+    print("Hola")
+else:
+    print("Adios")
+
