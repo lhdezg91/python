b8d06661f80f02f379993f500af7898c3d462015
Lian Hernandez <lian.hernandez91@gmail.com>
2025-03-19 23:07:04 -0300

    retos

---
+++ retos/retos.py
@@ -0,0 +1,12 @@
+def reto1():
+    # https://www.python.org/
+    """
+    Reto 1: Imprimir un mensaje en pantalla
+    """
+    texto = "Python"
+    numero = 10
+    booleano = True
+    flotante = 10.5
+    
+    print(f"!Hola, {texto}!!")
+
