815f09e3a1e356721a15e19c30304132c4fc79ed
Lian Hernandez <lian.hernandez91@gmail.com>
2025-03-22 21:04:24 -0300

    fun

---
+++ Intermedio/lambdas.py
@@ -0,0 +1,4 @@
+
+suma = lambda numero1, numero2: numero1 + numero2
+
+print(suma(5, 5)) # 10
--- retos/retos.py
+++ retos/retos.py
@@ -64,3 +64,62 @@ class Agenda:
             
         print("Contacto no encontrado")
         return None
+
+"""
+/*
+ * Escribe una función que reciba dos palabras (String) y retorne
+ * verdadero o falso (Bool) según sean o no anagramas.
+ * - Un Anagrama consiste en formar una palabra reordenando TODAS
+ *   las letras de otra palabra inicial.
+ * - NO hace falta comprobar que ambas palabras existan.
+ * - Dos palabras exactamente iguales no son anagrama.
+ */
+"""
+def es_anagrama(palabra1, palabra2):
+    #return sorted(palabra1) == sorted(palabra2)
+    palabra1 = palabra1.lower()
+    palabra2 = palabra2.lower()
+    
+    if len(palabra1) != len(palabra2):
+        return False
+    if palabra1 == palabra2:
+        return False
+    
+    for i in palabra1:
+        if i not in palabra2:
+            return False
+    
+    return True
+
+"""
+/*
+ * Escribe un programa que imprima los 50 primeros números de la sucesión
+ * de Fibonacci empezando en 0.
+ * - La serie Fibonacci se compone por una sucesión de números en
+ *   la que el siguiente siempre es la suma de los dos anteriores.
+ *   0, 1, 1, 2, 3, 5, 8, 13...
+ */
+"""
+
+def fibonacci():
+    a, b = 0, 1
+    for i in range(50):
+        print(a)
+        a, b = b, a + b
+
+def es_primo(num):
+    if num < 2:
+        return False
+    
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
+
+def invertir(cadena):
+    cadena2 = ""
+    for i in range(1, len(cadena)+1):
+        cadena2 = cadena2 + cadena[-i]
+    print(cadena2)
+
+invertir("hola")
